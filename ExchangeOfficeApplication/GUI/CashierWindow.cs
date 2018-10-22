fac034dee124443a0e58e30f652520461454ccb3
DanilaK <m98ail@icloud.com>
2018-10-22 03:04:07 +0300

    Skeleton CashierWindow was added.

--- ExchangeOfficeApplication/GUI/CashierWindow.cs
+++ ExchangeOfficeApplication/GUI/CashierWindow.cs
@@ -7,52 +7,31 @@ namespace ExchangeOfficeApplication.GUI
     /// Class Login
     /// 
     /// </summary>
-    public class Login
+    public class CashierWindow
     {
         [Builder.Object] private Label LoginLable;
         [Builder.Object] private Label PasswordLable;
         [Builder.Object] private Entry LoginEntry;
         [Builder.Object] private Entry PasswordEntry;
-        [Builder.Object] private Window LoginWindow;
+        [Builder.Object] private Window Cashier;
         
         private Builder GuiBuilder;
         
-        public Login()
+        public CashierWindow()
         {
             Gtk.Application.Init();
             GuiBuilder = new Builder();
             try
             {
                 GuiBuilder.AddFromFile(
-                    "./GUI/LoginWindow.glade");
+                    "./GUI/CashierWindow.glade");
                 GuiBuilder.Autoconnect(this);
-                LoginWindow.Visible = true;
-                Application.Run();
+                Cashier.Visible = true;
             }
             catch (Exception e)
             {
                 Console.WriteLine(e.Message);
             }
-        }
-        
-        protected void  OkButtonClicked(object sender, EventArgs a)
-        {
-            LoginWindow.Visible = false;
-            new CashierWindow();
-            Console.WriteLine("OK");
-            // TODO
-        }
-        
-        protected void  ExitButtonClicked(object sender, EventArgs a)
-        {
-            Console.WriteLine("Exit");
-            // TODO
-        }
-        
-        protected void  AdministratorSwitchActivate(object sender, ButtonReleaseEventArgs a)
-        {
-            Console.WriteLine("Admin");
-            // TODO
         }
         
         protected void  ExitButton(object sender, EventArgs a)
